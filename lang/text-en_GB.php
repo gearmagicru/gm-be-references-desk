9adcdb814a301bdbe6efb25dc4d1d6c9e3d9b809
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-12 11:06:41 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'References Panel',
+    '{description}' => 'Panel for displaying references elements',
+    '{permissions}' => [
+        'any'  => ['Full access', 'View and read panel items'],
+        'view' => ['View', 'Panel view'],
+        'read' => ['Reading', 'Reading panel items']
+    ]
+];
